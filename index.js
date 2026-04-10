d34d91fe335e53ee00347d1fba6223bdbc8ced4e
dvzin0770-rgb <dvzin0770@gmail.com>
2026-04-10 18:28:56 -0300

    Set up Discord bot with message handling

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log("bot");
+const { Client, GatewayIntentBits } = require('discord.js');
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent
+  ]
+});
+
+client.on('ready', () => {
+  console.log('Bot online!');
+});
+
+client.on('messageCreate', (message) => {
+  if (message.content === "!oi") {
+    message.reply("Funcionando!");
+  }
+});
+
+client.login(process.env.TOKEN);
